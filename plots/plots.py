8bdc3863c9cf22a9cb8eb3af23a889d9b792a808
Gouranga Kole <gouranga.tifr@gmail.com>
2020-06-04 15:36:39 +0300

    update on plotting script

--- plots/plots.py
+++ plots/plots.py
@@ -10,12 +10,15 @@ x, y = loadtxt('../hist.dat',unpack=True, usecols=[0,1])
 #fit = np.polyfit(x,y,1)
 #fit_fn = np.poly1d(fit) 
 # fit_fn is now a function which takes in x and returns an estimate for y
+#plt.plot(x,y, 'x','go', label="points") #x,y, 'go', x, fit_fn(x), '--k', label="test0"
 
-#plt.plot(x,y, 'go', label="points") #x,y, 'go', x, fit_fn(x), '--k', label="test0"
-
-plt.scatter(x,y, c='r', label='the data')
-plt.xlabel("x values")
-plt.ylabel("y values")
+# plt.hist(x)
+# https://jakevdp.github.io/PythonDataScienceHandbook/04.02-simple-scatter-plots.html
+# plt.scatter(x,y, marker='*',c='b', label='Pythia8')
+plt.plot(x,y, '-^k', c='b', label='Pythia8')
+plt.xlabel(r"Cos $\theta^{*}$")
+# plt.ylabel(r"$\dot{\Theta}$[deg/s]")
+plt.ylabel("Events")
 plt.legend(loc=2)
 #plt.show()
-plt.savefig("theta_tc.png")
+plt.savefig("theta_tc.pdf")
